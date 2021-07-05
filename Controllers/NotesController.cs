b67c822ff72b9282b156781550533afe5eda6ee9
Oleh-Ozhybko <olehozhybko@gmail.com>
2021-07-05 16:43:57 +0300

    Add project files.

---
+++ Controllers/NotesController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NoteCore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace NoteCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotesController : ControllerBase
+    {
+        NoteContext noteContext;
+        public NotesController(NoteContext context)
+        {
+            noteContext = context;
+            if (!noteContext.Notes.Any())
+            {
+                noteContext.Notes.Add(new Note { CreationDate = DateTime.Now, Description = "Desc", Title ="first" });
+                noteContext.Notes.Add(new Note { CreationDate = DateTime.Now, Description = "Desc", Title = "second" });
+                noteContext.SaveChanges();
+            }
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Note>>> Get()
+        {
+            return await noteContext.Notes.ToListAsync();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Note>> Get(int id)
+        {
+            Note note = await noteContext.Notes.FirstOrDefaultAsync(i => i.Id == id);
+            if (note == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(note);
+        }
+        [HttpPost]
+        public async Task<ActionResult<Note>> Post(Note note)
+        {
+            if (note == null)
+                return BadRequest();
+            noteContext.Notes.Add(note);
+            await noteContext.SaveChangesAsync();
+
+            return Ok(note);
+        }
+        [HttpPut]
+        public async Task<ActionResult<Note>> Put(Note note)
+        {
+            if (note == null)
+                return BadRequest();
+         
+            if (!noteContext.Notes.Any(i => i.Id == note.Id))
+                return NotFound();
+
+            noteContext.Update(note);
+            await noteContext.SaveChangesAsync();
+            return Ok(note);
+        }
+        [HttpDelete]
+        public async Task<ActionResult<Note>> Delete(int id)
+        {
+            Note note = noteContext.Notes.FirstOrDefault(i => i.Id == id);
+            if (note == null)
+                return NotFound();
+
+            noteContext.Notes.Remove(note);
+            await noteContext.SaveChangesAsync();
+            return Ok(note);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NoteCore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace NoteCore
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            string connection = @"Server=(localdb)\MSSQLLocalDB;Database=NotesDb;Trusted_Connection=True;";
+            services.AddDbContext<NoteContext>(options => options.UseSqlServer(connection));
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
